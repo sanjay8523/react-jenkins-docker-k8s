916d0f1cf1ea6f6f4047ea8f16ccc5948aa514f3
sanjay8523 <sanjaympatel8523@gmail.com>
2024-11-09 21:01:54 +0530

    final commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App with CI/CD Pipeline</h1>
+      <p>This React app is deployed using Jenkins, Docker, and Kubernetes.</p>
+    </div>
+  );
+}
+
+export default App;
